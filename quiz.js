92e567772842f890e7e6fe8ef3a469ed4dac09b1
Mudath-thir-HM <eighthmudathir@gmail.com>
2024-09-30 11:01:57 +0100

    added an event listener to read the score on another page

--- quiz.js
+++ quiz.js
@@ -113,4 +113,8 @@ function clearStatusClass(element) {
     element.classList.remove('correct')
     element.classList.remove('wrong')
   }
-  
+
+  // Event listener for viewing the score on a separate page
+viewScoreButton.addEventListener('click', () => {
+    window.location.href = 'score.html' // Redirect to score page
+  })
